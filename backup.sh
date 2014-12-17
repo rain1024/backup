d262c7c047174d77426985ef59caa2d239a1c47c
Vu Anh <brother.rain.1024@gmail.com>
2014-12-17 09:50:18 +0700

    backup

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ do
 echo "Copy $file"
 cp $file -t dotfiles/
 done
+echo $(date +"%H:%M:%S %d-%m-%Y") >> log
 git add -A
 git commit -m "backup"
 git push origin master
-echo $(date +"%H:%M:%S %d-%m-%Y") >> log
